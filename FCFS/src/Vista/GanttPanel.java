22e4e5f4af6e47968f62d207aabf163eb15911a8
CristianC4 <31807256+CristianC4@users.noreply.github.com>
2019-06-05 21:44:25 -0500

    Esta vez si son los .java XD

--- FCFS/src/Vista/GanttPanel.java
+++ FCFS/src/Vista/GanttPanel.java
@@ -9,6 +9,7 @@ import javax.swing.table.DefaultTableModel;
 public class GanttPanel extends TablePanel {
 
 	private final String[] HEADER = { "PID" };
+	private TableCellRedendererColor cellRenderer;
 
 	public GanttPanel(JFrame window, String title) {
 		super(window, title);
@@ -16,6 +17,7 @@ public class GanttPanel extends TablePanel {
 		getTable().setBackground(SystemColor.window);
 		getTable().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		setTableModel(new DefaultTableModel(HEADER, 0));
+		cellRenderer = new TableCellRedendererColor();
 	}
 
 	// TODO: Nada elegante este metodo
@@ -25,11 +27,12 @@ public class GanttPanel extends TablePanel {
 		getTableModel().setValueAt(process[0], row, 0);
 		paintWaitTime(row, (int) process[1], (int) process[3]);
 		paintBurstTime(row, (int) process[3], (int) process[4]);
+		getTable().setDefaultRenderer(Object.class , cellRenderer);
 	}
 	
 	private void paintWaitTime(int row, int arrivalTime, int startTime){
 		for (int i = arrivalTime; i < startTime; i++) {
-			getTableModel().setValueAt("---", row, i + 1);			
+			getTableModel().setValueAt("---", row, i + 1);	
 		}
 	}
 	
