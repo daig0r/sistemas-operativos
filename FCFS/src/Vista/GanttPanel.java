349802e6d2e227ea8a4008620f503fdde904705f
Diego Alejandro Rodriguez Ayala <diego.magno120@gmail.com>
2019-06-05 21:54:20 -0500

    Modificado gitignore

--- FCFS/src/Vista/GanttPanel.java
+++ FCFS/src/Vista/GanttPanel.java
@@ -8,28 +8,24 @@ import javax.swing.table.DefaultTableModel;
 
 public class GanttPanel extends TablePanel {
 
-	private final String[] HEADER = { "PID" };
-
 	public GanttPanel(JFrame window, String title) {
 		super(window, title);
 		getTable().setShowGrid(false);
 		getTable().setBackground(SystemColor.window);
 		getTable().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		setTableModel(new DefaultTableModel(HEADER, 0));
+		setTableModel(new DefaultTableModel(new Object[]{"PID"}, 0));
 	}
 
-	// TODO: Nada elegante este metodo
 	public void paintProcess(Object[] process) {
 		int row = getTableModel().getRowCount();
-		getTableModel().addRow(HEADER);
-		getTableModel().setValueAt(process[0], row, 0);
-		paintWaitTime(row, (int) process[1], (int) process[3]);
+		getTableModel().addRow(new Object[]{process[0]});
 		paintBurstTime(row, (int) process[3], (int) process[4]);
+		paintWaitTime(row, (int) process[1], (int) process[3]);
 	}
 	
-	private void paintWaitTime(int row, int arrivalTime, int startTime){
+	private void paintWaitTime(int row, int arrivalTime, int startTime) {
 		for (int i = arrivalTime; i < startTime; i++) {
-			getTableModel().setValueAt("---", row, i + 1);			
+			getTableModel().setValueAt("---", row, i + 1);
 		}
 	}
 	
@@ -38,7 +34,5 @@ public class GanttPanel extends TablePanel {
 			getTableModel().addColumn(String.valueOf(i));
 			getTableModel().setValueAt("**", row, i + 1);
 		}
-	}
-	
-	
+	}	
 }
