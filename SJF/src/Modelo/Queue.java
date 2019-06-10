cf33c4f0acff3c5c7145699d9f06d7ef057154a4
Diego Alejandro Rodriguez Ayala <diego.magno120@gmail.com>
2019-06-10 06:57:30 -0500

    Añadido metodo para eliminar un nodo específico de la cola

--- SJF/src/Modelo/Queue.java
+++ SJF/src/Modelo/Queue.java
@@ -3,35 +3,72 @@ package Modelo;
 public class Queue<T> {
 
 	private Node<T> head, tail;
+	private int size;
 
 	public Queue() {
 		head = tail = null;
 	}
 
 	public void add(T data) {
-
 		Node<T> node = new Node<T>();
 		node.setData(data);
 		node.setNext(null);
-
 		if (head == null) {
 			head = node;
 		} else
 			tail.setNext(node);
-
 		tail = node;
+		size++;
 	}
-	
-	public T poll() {
 
+	public T poll() {
 		T data;
 		Node<T> aux = head;
-
 		head = aux.getNext();
 		data = aux.getData();
 		aux = null;
-
+		size--;
 		return data;
+	}
+	
+	public T remove(int pos) {
+		if (size >= pos) {
+			T data;
+			Node<T> aux = head;			
+			if (pos == 1) {
+				head = aux.getNext();
+				data = aux.getData();
+				aux = null;
+			} else {
+				for (int i = 1; i < pos - 1; i++) {
+					aux = aux.getNext();					
+				}
+				Node<T> temp = aux.getNext();
+				aux.setNext(temp.getNext());
+				data = temp.getData();
+				temp = null;
+			}
+			size--;
+			return data;
+		}
+		return null;
+	}
+
+	public T getData(int pos) {
+		Node<T> aux = head;
+		int p = 1;
+		while ((p < pos) && (aux != null)) {
+			aux = aux.getNext();
+			p++;
+		}
+		if (aux != null)
+			return aux.getData();
+
+		return aux.getData();
+	}
+
+	public int getSize() {
+		return size;
 	}
 
 	public boolean isEmpty() {
@@ -42,15 +79,12 @@ public class Queue<T> {
 
 	@Override
 	protected void finalize() throws Throwable {
-
 		Node<T> aux;
-
 		while (head != null) {
 			aux = head;
 			head = aux.getNext();
 			aux = null;
 		}
-
 		head = null;
 		System.gc();
 	}
